924b2387c18d1308008536f40412db1aea7f7d54
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-01-25 18:41:16 +0000

    Style TracklistItem Componend

--- src/components/TracklistItem.jsx
+++ src/components/TracklistItem.jsx
@@ -3,10 +3,10 @@ import styles from "../css/modules/_TracklistItem.module.css";
 
 const TracklistItem = ({ src, name, artist, album }) => {
   return (
-    <li>
+    <li className={styles.tracklistItem}>
       <div className={styles.infoContainer}>
         <div className={styles.imgContainer}>
-          <img src={src} alt="Album Cover" />
+          <img className={styles.albumImg} src={src} alt="Album Cover" />
         </div>
         <div className={styles.titleContainer}>
           <h4 className={styles.songTitle}>{name}</h4>
