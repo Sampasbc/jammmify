c1d4de266fccbbbc9aa6ab9536cddb2b449d7e50
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-01-27 10:47:08 +0000

    Add logic for showing TrackDetails when clicking TracklistItem

--- src/components/TracklistItem.jsx
+++ src/components/TracklistItem.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../css/modules/_TracklistItem.module.css";
 import { FaPlusCircle } from "react-icons/fa";
+import TrackDetails from "./TrackDetails";
 
 const TracklistItem = ({ src, name, artist, album }) => {
-  const handleClick = () => {};
+  const [isActive, setIsActive] = useState(false);
+  const handleActive = (active) => {
+    setIsActive((prev) => !prev);
+  };
+
+  const handleAddBtn = () => {
+    const music = {
+      name: name,
+      artist: artist,
+      album: album,
+      album_cover: src,
+    };
+    console.log(JSON.stringify(music));
+  };
 
   return (
     <li className={styles.tracklistItem}>
-      <div className={styles.infoContainer}>
-        <div className={styles.imgContainer}>
-          <img className={styles.albumImg} src={src} alt="Album Cover" />
+      <div className={styles.tracklistItemContainer} onClick={handleActive}>
+        <div className={styles.infoContainer}>
+          {!isActive && (
+            <div className={styles.imgContainer}>
+              <img className={styles.albumImg} src={src} alt="Album Cover" />
+            </div>
+          )}
+          <div className={styles.titleContainer}>
+            <h4 className={styles.songTitle}>{name}</h4>
+            <h5 className={styles.songArtist}>{artist}</h5>
+          </div>
         </div>
-        <div className={styles.titleContainer}>
-          <h4 className={styles.songTitle}>{name}</h4>
-          <h5 className={styles.songArtist}>{artist}</h5>
-        </div>
+        <h5 className={styles.songAlbum}>{album}</h5>
+        <button className="btn_secondary" onClick={handleAddBtn}>
+          <FaPlusCircle />
+          add
+        </button>
       </div>
-      <h5 className={styles.songAlbum}>{album}</h5>
-      <button className="btn_secondary" onClick={handleClick}>
-        <FaPlusCircle />
-        add
-      </button>
+      {isActive && <TrackDetails />}
     </li>
   );
 };
